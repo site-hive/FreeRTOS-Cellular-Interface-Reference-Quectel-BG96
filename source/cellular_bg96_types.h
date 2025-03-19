a00a3d00f41e52856533d494ce96ba0af13cd46b
Anthony Zeater <anthony@sitehive.co>
2025-03-19 17:51:46 +1100

    Added missing file

---
+++ source/cellular_bg96_types.h
@@ -0,0 +1,12 @@
+#ifndef __CELLULAR_BG96_TYPES_H__
+#define __CELLULAR_BG96_TYPES_H__
+
+// @brief Enum representing type of module currently in use
+typedef enum CellularModuleType
+{
+    CELLULAR_MODULE_TYPE_UNKNOWN = 0,
+    CELLULAR_MODULE_TYPE_BG96, 
+    CELLULAR_MODULE_TYPE_EG21G,
+} CellularModuleType_t;
+
+#endif
